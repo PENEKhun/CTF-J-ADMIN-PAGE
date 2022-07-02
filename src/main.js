6b649e3e2d0ff45f87c58ef30afe75fd0dd07072
PENEKhun <penekhun@gmail.com>
2022-07-03 04:42:07 +0900

    etc : 뷰 앱에서 vue-load-script use

--- src/main.js
+++ src/main.js
@@ -2,16 +2,14 @@ import { createApp } from 'vue'
 import Vuex from "vuex"
 import App from '@/App.vue'
 import Auth from "@/store/auth"
-
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
- import {router} from "@/router";
+import LoadScript from "vue-plugin-load-script";
+import {router} from "@/router";
 
 
 let app = createApp(App);
  app.config.globalProperties.Auth = Auth;
 
+app.use(LoadScript);
 app.use(router);
 app.use(Vuex);
 app.mount('#app');
