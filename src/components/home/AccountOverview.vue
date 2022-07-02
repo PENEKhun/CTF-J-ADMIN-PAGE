6510b693949d235875acdf2797f0e2e0254a656f
PENEKhun <penekhun@gmail.com>
2022-07-03 04:37:39 +0900

    first Commit

---
+++ src/components/home/AccountOverview.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container-xxl flex-grow-1 container-p-y">
+    <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Accounts /</span> Overview</h4>
+
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card mb-4">
+          <h5 class="card-header">title</h5>
+          <!-- Account -->
+          <div class="card-body">
+            content
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+
+import Content from "@/components/common/CommonContent";
+
+export default {
+  name: "DefaultHome",
+  components: [
+    Content,
+  ]
+}
+</script>
