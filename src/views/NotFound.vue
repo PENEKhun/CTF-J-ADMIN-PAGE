8bdcd2e21a2227183a6f6d54ab24972921733f37
PENEKhun <penekhun@gmail.com>
2022-07-03 04:38:03 +0900

    etc : 404 페이지 변경

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,8 +1,25 @@
 <template>
-  <div>
-    <label>페이지를 찾을 수 없습니다.</label>
-    <img src="@/assets/404.jpg" alt="page not found"/>
+  <!-- Content -->
+
+  <!-- Error -->
+  <div class="container-xxl container-p-y">
+    <div class="misc-wrapper">
+      <h2 class="mb-2 mx-2">Page Not Found :(</h2>
+      <p class="mb-4 mx-2">Oops! 😖 The requested URL was not found on this server.</p>
+      <a href="/" class="btn btn-primary">Back to home</a>
+      <div class="mt-3">
+        <img
+            src="../assets/img/page-misc-error-light.png"
+            alt="page-misc-error-light"
+            width="500"
+            class="img-fluid"
+            data-app-dark-img="illustrations/page-misc-error-dark.png"
+            data-app-light-img="illustrations/page-misc-error-light.png"
+        />
+      </div>
+    </div>
   </div>
+
 </template>
 
 <script>
@@ -10,5 +27,3 @@ export default {
 
 };
 </script>
-
-<style></style>
