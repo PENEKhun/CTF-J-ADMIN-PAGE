06612720fce2ed023b6260d3670729955e287b28
PENEKhun <penekhun@gmail.com>
2022-07-04 01:50:04 +0900

    etc : App 뷰 간소화 수정

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,8 @@
 <template>
-  <div id="app">
     <router-view></router-view>
-  </div>
 </template>
 
 <script>
-
 import Main from "@/views/Main";
 
 export default {
@@ -14,13 +11,3 @@ export default {
 }
 
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-</style>
