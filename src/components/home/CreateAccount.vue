cba47b440b3575515021cf426b49d15389cd4ce2
PENEKhun <penekhun@gmail.com>
2022-07-04 01:51:23 +0900

    first Commit

--- src/components/home/CreateAccount.vue
+++ src/components/home/CreateAccount.vue
@@ -3,18 +3,68 @@
     <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Accounts /</span> Create Account</h4>
 
     <div class="row">
-      <div class="col-md-12">
+      <!-- Basic Layout -->
+      <div class="col-xxl">
         <div class="card mb-4">
-          <h5 class="card-header">title</h5>
-          <!-- Account -->
+          <div class="card-header d-flex align-items-center justify-content-between">
+            <h5 class="mb-0">Fill the blank to create account</h5>
+            <small class="text-muted float-end">회원정보를 입력해주세요</small>
+          </div>
           <div class="card-body">
-            content
+            <form>
+              <div class="row mb-3">
+                <label class="col-sm-2 col-form-label" for="basic-default-name">Name</label>
+                <div class="col-sm-10">
+                  <input type="text" class="form-control" id="basic-default-name" placeholder="Moon Seonghun">
+                </div>
+              </div>
+              <div class="row mb-3">
+                <label class="col-sm-2 col-form-label" for="basic-default-company">Company</label>
+                <div class="col-sm-10">
+                  <input type="text" class="form-control" id="basic-default-company" placeholder="ACME Inc.">
+                </div>
+              </div>
+              <div class="row mb-3">
+                <label class="col-sm-2 col-form-label" for="basic-default-email">Email</label>
+                <div class="col-sm-10">
+                  <div class="input-group input-group-merge">
+                    <input type="text" id="basic-default-email" class="form-control" placeholder="penekhun@gmail.com" aria-label="penekhun@gmail.com" aria-describedby="basic-default-email2">
+<!--                    <span class="input-group-text" id="basic-default-email2">@example.com</span>-->
+                  </div>
+                  <div class="form-text">You can use letters, numbers &amp; periods</div>
+                </div>
+              </div>
+              <div class="row mb-3">
+                <label for="exampleFormControlSelect1" class="col-sm-2 col-form-label">User Role</label>
+                <select class="form-select" id="exampleFormControlSelect1" aria-label="Default select example">
+                  <option value="1">User</option>
+                  <option value="2">Admin</option>
+                </select>
+              </div>
+
+              <div class="row mb-3">
+                <label class="col-sm-2 col-form-label" for="basic-default-phone">Phone No</label>
+                <div class="col-sm-10">
+                  <input type="text" id="basic-default-phone" class="form-control phone-mask" placeholder="010-xxx..." aria-label="658 799 8941" aria-describedby="basic-default-phone">
+                </div>
+              </div>
+              <div class="row mb-3">
+                <label class="col-sm-2 col-form-label" for="basic-default-message">Message</label>
+                <div class="col-sm-10">
+                  <textarea id="basic-default-message" class="form-control" placeholder="Hi, Do you have a moment to talk Joe?" aria-label="Hi, Do you have a moment to talk Joe?" aria-describedby="basic-icon-default-message2"></textarea>
+                </div>
+              </div>
+              <div class="row justify-content-end">
+                <div class="col-sm-10">
+                  <button type="submit" class="btn btn-primary">process</button>
+                </div>
+              </div>
+            </form>
           </div>
         </div>
       </div>
     </div>
   </div>
-
 </template>
 
 <script>
