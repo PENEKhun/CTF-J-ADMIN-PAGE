88a0f29f77ad7a1f5af6b6307e42232be0e2808b
PENEKhun <penekhun@gmail.com>
2022-07-04 01:52:13 +0900

    feat : api를위한 axios 관리하는 index.js 생성

---
+++ src/api/index.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import { setInterceptors } from './common/interceptors';
+import loginStore from "@/store/auth";
+import Auth from "@/store/auth";
+
+const HOST = "http://15.165.86.75:8080/api/v1/admin";
+
+export function createInstance() {
+    return axios.create({
+        baseURL: HOST
+    });
+}
+
+export function createInstanceWithAuth(url) { // Token값과 특정 url을 붙여서 셋팅
+    let strTokenExpired = loginStore.state.tokenExpired;
+    strTokenExpired = strTokenExpired.replace("KST", "");
+    let tokenExpired = Date.parse(strTokenExpired);
+    let now = new Date().getTime();
+    if (now > tokenExpired) {
+        // 토큰 만료시
+        console.log("토큰을 재발급합니다.");
+        Auth.dispatch("doReissue").then(() => {
+            const instance = axios.create({
+                baseURL: `${HOST}${url}`,
+            })
+            return setInterceptors(instance);
+        }).catch((err) => {
+            console.error("토큰 재발급 오류 ", err);
+        });
+    } else {
+        // 토큰 만료가 아니면
+        const instance = axios.create({
+            baseURL: `${HOST}${url}`,
+        })
+        return setInterceptors(instance);
+    }
+}
+
+export const instance = createInstance();
+
+// export const posts = createInstanceWithAuth('/posts');
+export const problemAxios = createInstanceWithAuth('/problem');
+// export const file = createInstanceWithAuth('/file');
+export const logAxios = createInstanceWithAuth('/log');
