036532f3d21c2d0719e42c342abe46b47294e15c
PENEKhun <penekhun@gmail.com>
2022-07-05 02:55:00 +0900

    feat : 플래그 인증 로그 로직 페이징 처리

--- src/components/home/FlagAuthLog.vue
+++ src/components/home/FlagAuthLog.vue
@@ -3,7 +3,9 @@
     <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Log /</span> Flag</h4>
 
     <div class="card">
-      <!--      <h5 class="card-header"></h5>-->
+
+      <CommonPaging :search="search" :total-elements="totalElements" :total-page="totalPage"/>
+
       <div class="table-responsive text-nowrap">
         <table class="table table-striped">
           <thead>
@@ -16,8 +18,8 @@
           </tr>
           </thead>
           <tbody class="table-border-bottom-0">
-          <tr v-for="(line, index) in log" :key="index++">
-            <td> {{index}} </td>
+          <tr v-for="line in log" :key="line['id']">
+            <td> {{ line['id'] }} </td>
             <td><i class="fab fa-angular fa-lg text-danger me-3"></i>
               {{ line['action'] }}
             </td>
@@ -34,6 +36,10 @@
           </tbody>
         </table>
       </div>
+
+
+      <CommonPaging :search="search" :total-elements="totalElements" :total-page="totalPage"/>
+
     </div>
   </div>
 
@@ -41,27 +47,45 @@
 
 <script>
 
-import Content from "@/components/common/CommonContent";
 import {Log} from "@/api/log";
+import CommonPaging from "@/components/common/log/CommonPaging";
 
 export default {
-  name: "DefaultHome",
-  components: [
-    Content,
-  ],
+  name: "FlagAuthLog",
+  components: {
+    'CommonPaging': CommonPaging
+  },
   data: function() {
     return {
-      log : Array
+      log : Array,
+      search : {
+        logType: "flag",
+        sortType: "asc",
+        pageNum: 0,
+        amount: 30,
+      },
+
+      totalPage : 0,
+      totalElements : 0,
     }
+  },
+  watch:{
+    // 페이징 관련 변수가 변경 될 경우
+    search: {
+      deep: true,
+      immediate: true,
+      handler : 'getAllFlagLog'
+    },
   },
   methods : {
     getAllFlagLog() {
-      let res = Log.fetchFlagLog("flag", 1, 30);
+      let res = Log.fetchFlagLog(this.search.logType, this.search.pageNum, this.search.amount, this.search.sortType);
       console.log(res);
       res.then((q) => {
         if (q.status === 200){
-          console.log("loaded");
-          this.$data.log = q.data['log'];
+          this.log = q.data['log'];
+          this.totalPage = q.data['totalPage'] - 1;
+          this.totalElements = q.data['totalElements'];
           console.log(this.$data.log);
         }
       }).catch((err) => {
@@ -69,8 +93,5 @@ export default {
       })
     }
   },
-  beforeMount() {
-    this.getAllFlagLog();
-  }
 }
 </script>
