5b959e4ad8f6043da7cc9f613d9d5b23efbc2e00
PENEKhun <penekhun@gmail.com>
2022-07-04 19:44:15 +0900

    feat : 파일 업로드를 위한 axios 생성

--- src/api/problem.js
+++ src/api/problem.js
@@ -1,7 +1,10 @@
-import {problemAxios} from "@/api/index";
+import {fileAxios, problemAxios} from "@/api/index";
 
 export const Problem = {
     fetchProblem: async () => {
         return await problemAxios.get();
+    },
+    uploadFile: async (file) => {
+        return await fileAxios.post("", {file}, {headers : {'Content-Type' : 'multipart/form-data'}});
     }
 }
