06b49c294b2f671dd64be9e84d0bc16bc8926289
PENEKhun <penekhun@gmail.com>
2022-07-05 04:08:33 +0900

    etc : 계정 생성 UI 변경

--- src/components/home/CreateAccount.vue
+++ src/components/home/CreateAccount.vue
@@ -13,50 +13,54 @@
           <div class="card-body">
             <form>
               <div class="row mb-3">
-                <label class="col-sm-2 col-form-label" for="basic-default-name">Name</label>
-                <div class="col-sm-10">
-                  <input type="text" class="form-control" id="basic-default-name" placeholder="Moon Seonghun">
+                <label class="col-sm-2 col-form-label" for="basic-default-name">Username</label>
+                <div class="col-sm-4">
+                  <input type="text" class="form-control" id="basic-default-name" placeholder="penekhun">
+                  <div class="form-text">also known as 'ID'</div>
+                </div>
+
+                <label class="col-sm-2 col-form-label" for="basic-default-name">Password</label>
+                <div class="col-sm-4">
+                  <input type="text" class="form-control" id="basic-default-name" placeholder="********">
+                  <div class="form-text">password 규칙</div>
                 </div>
               </div>
+
               <div class="row mb-3">
-                <label class="col-sm-2 col-form-label" for="basic-default-company">Company</label>
-                <div class="col-sm-10">
-                  <input type="text" class="form-control" id="basic-default-company" placeholder="ACME Inc.">
+                <label class="col-sm-2 col-form-label" for="basic-default-name">Nickname</label>
+                <div class="col-sm-4">
+                  <input type="text" class="form-control" id="basic-default-name" placeholder="PeNeKKKkkkkk">
+                </div>
+
+                <label class="col-sm-2 col-form-label" for="basic-default-name">RealName</label>
+                <div class="col-sm-4">
+                  <input type="text" class="form-control" id="basic-default-name" placeholder="MoonSeonghun">
                 </div>
               </div>
+
+
               <div class="row mb-3">
                 <label class="col-sm-2 col-form-label" for="basic-default-email">Email</label>
-                <div class="col-sm-10">
+                <div class="col-sm-4">
                   <div class="input-group input-group-merge">
                     <input type="text" id="basic-default-email" class="form-control" placeholder="penekhun@gmail.com" aria-label="penekhun@gmail.com" aria-describedby="basic-default-email2">
 <!--                    <span class="input-group-text" id="basic-default-email2">@example.com</span>-->
                   </div>
                   <div class="form-text">You can use letters, numbers &amp; periods</div>
                 </div>
-              </div>
-              <div class="row mb-3">
+
                 <label for="exampleFormControlSelect1" class="col-sm-2 col-form-label">User Role</label>
+                <div class="col-sm-4">
                 <select class="form-select" id="exampleFormControlSelect1" aria-label="Default select example">
                   <option value="1">User</option>
                   <option value="2">Admin</option>
                 </select>
+                </div>
               </div>
 
-              <div class="row mb-3">
-                <label class="col-sm-2 col-form-label" for="basic-default-phone">Phone No</label>
-                <div class="col-sm-10">
-                  <input type="text" id="basic-default-phone" class="form-control phone-mask" placeholder="010-xxx..." aria-label="658 799 8941" aria-describedby="basic-default-phone">
-                </div>
-              </div>
-              <div class="row mb-3">
-                <label class="col-sm-2 col-form-label" for="basic-default-message">Message</label>
-                <div class="col-sm-10">
-                  <textarea id="basic-default-message" class="form-control" placeholder="Hi, Do you have a moment to talk Joe?" aria-label="Hi, Do you have a moment to talk Joe?" aria-describedby="basic-icon-default-message2"></textarea>
-                </div>
-              </div>
               <div class="row justify-content-end">
-                <div class="col-sm-10">
-                  <button type="submit" class="btn btn-primary">process</button>
+                <div class="col-sm-12">
+                  <button type="submit" class="btn btn-primary">submit</button>
                 </div>
               </div>
             </form>
