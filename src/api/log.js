7a017fe7eef3d1d10372be746c6a62186bc44d8b
PENEKhun <penekhun@gmail.com>
2022-07-05 02:56:27 +0900

    feat : 통합 로그 axios 생성

--- src/api/log.js
+++ src/api/log.js
@@ -1,7 +1,10 @@
 import {logAxios} from "@/api/index";
 
 export const Log = {
-    fetchFlagLog: async (_logType, _pageNum, _amount) => {
-        return logAxios.get(`?logType=${_logType}&pageNum=${_pageNum}&amount=${_amount}`);
+    fetchFlagLog: async (_logType, _pageNum, _amount, _sortType) => {
+        return logAxios.get(`?logType=${_logType}&pageNum=${_pageNum}&amount=${_amount}&sortType=${_sortType}`);
+    },
+    fetchIntegratedLog: async (_logType, _pageNum, _amount, _sortType) => {
+        return logAxios.get(`?logType=${_logType}&pageNum=${_pageNum}&amount=${_amount}&sortType=${_sortType}`);
     }
 }
