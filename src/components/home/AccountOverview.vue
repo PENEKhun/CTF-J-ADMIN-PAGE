328692bc11f088639ce3327659f72760bd0ce404
PENEKhun <penekhun@gmail.com>
2022-07-05 22:13:54 +0900

    feat : AccountOverview 컴포넌트 기본 로직 작성

--- src/components/home/AccountOverview.vue
+++ src/components/home/AccountOverview.vue
@@ -2,24 +2,86 @@
   <div class="container-xxl flex-grow-1 container-p-y">
     <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Accounts /</span> Overview</h4>
 
-    <div class="row">
-      <div class="col-md-12">
-        <div class="card mb-4">
-          <h5 class="card-header">title</h5>
-          <!-- Account -->
-          <div class="card-body">
-            content
-          </div>
-        </div>
+
+    <!--    overView-->
+    <div v-if="$route.query.id == null" class="card">
+      <CommonPaging :search="search" :total-elements="totalElements" :total-page="totalPage"/>
+      <div class="table-responsive text-nowrap">
+        <table class="table table-striped">
+          <thead>
+          <tr>
+            <th>#</th>
+            <th>id</th>
+            <th>nickname</th>
+            <th>email</th>
+            <th>role</th>
+            <th>score</th>
+          </tr>
+          </thead>
+          <tbody class="table-border-bottom-0">
+          <tr v-for="(line) in accounts" :key="index=line['id']">
+            <td> {{ line['id'] }} </td>
+            <td><i class="fab fa-angular fa-lg text-danger me-3"></i>
+              <a style="color:black" :href='"./AccountOverview?id="+index'>{{ line['username'] }} </a>
+            </td>
+            <td >{{ line['nickname'] }}</td>
+            <td>
+              {{ line['email'] }}
+            </td>
+
+            <td v-if=" line['role'] === 'ADMIN'"><span class="badge bg-label-primary me-1">ADMIN</span></td>
+            <td v-else><span class="badge bg-label-warning me-1">USER</span></td>
+
+            <td>
+              {{ line['score'] }}
+            </td>
+
+            <td>
+              <div class="dropdown">
+                <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown" aria-expanded="false">
+                  <i class="bx bx-dots-vertical-rounded"></i>
+                </button>
+                <div class="dropdown-menu" style="">
+                  <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-edit-alt me-1"></i> Edit</a>
+                  <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-trash me-1"></i> Delete</a>
+                </div>
+              </div>
+            </td>
+
+          </tr>
+          </tbody>
+        </table>
       </div>
     </div>
+
+    <!--    Preview-->
+    <div v-else class="card p-4">
+
+      <div v-if="findAccountFromArr() !== undefined"
+          :key="account = findAccountFromArr()">
+
+        <!--           :key="account=accounts.find((acc) => acc.id === $route.query.id)">-->
+        <!--        문제가 존재할때-->
+
+        <strong>index</strong> : {{account['id']}} <br/>
+        <strong>username</strong> : {{account['username']}} <br/>
+        <strong>nickname</strong> : {{account['nickname']}} <br/>
+        <strong>email</strong> : {{account['email']}} <br/>
+        <strong>score</strong> : {{account['score']}} <br/>
+        <strong>role</strong> : {{account['role']}} <br/>
+        <strong>solved</strong> : {{account['solved']}} <br/>
+      </div>
+
+    </div>
+
   </div>
 
 </template>
 
 <script>
 
-import Content from "@/components/common/CommonContent";
+import CommonPaging from "@/components/common/log/CommonPaging";
+import {Account} from "@/api/account";
 
 export default {
   name: "AccountOverview",
