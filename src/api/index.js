4aea1007e02a20f99a565b9246a2b7622c064a98
PENEKhun <penekhun@gmail.com>
2022-07-06 07:58:30 +0900

    feat : account Axios 생성

--- src/api/index.js
+++ src/api/index.js
@@ -38,5 +38,6 @@ export const instance = createInstance();
 
 // export const posts = createInstanceWithAuth('/posts');
 export const problemAxios = createInstanceWithAuth('/problem');
+export const accountAxios = createInstanceWithAuth('/account');
 export const fileAxios = createInstanceWithAuth('/file');
 export const logAxios = createInstanceWithAuth('/log');
