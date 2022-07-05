f9c966c9b19273303a35e4a625839df036494dbd
PENEKhun <penekhun@gmail.com>
2022-07-06 01:35:17 +0900

    feat : 회원정보 수정 기능 개발

--- src/api/account.js
+++ src/api/account.js
@@ -5,4 +5,7 @@ export const Account = {
     fetchAccount: async (search) => {
         return accountAxios.get(pagingQuery(search));
     },
+    editPartly: async (account) => {
+        return accountAxios.patch("" + account.id, account, {headers : { "Content-Type": "multipart/form-data"}});
+    }
 }
--- src/components/home/CreateAccount.vue
+++ src/components/home/CreateAccount.vue
@@ -1,40 +1,48 @@
 <template>
   <div class="container-xxl flex-grow-1 container-p-y">
-    <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Accounts /</span> Create Account</h4>
+    <h4 v-if="!isEditMode" class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Accounts /</span> Create Account</h4>
 
     <div class="row">
       <!-- Basic Layout -->
       <div class="col-xxl">
         <div class="card mb-4">
           <div class="card-header d-flex align-items-center justify-content-between">
-            <h5 class="mb-0">Fill the blank to create account</h5>
-            <small class="text-muted float-end">회원정보를 입력해주세요</small>
+            <h5 v-if="isEditMode" class="mb-0">Change the information to edit account</h5>
+            <h5 v-else class="mb-0">Fill the blank to create account</h5>
+
+            <small v-if="isEditMode" class="text-muted float-end">정보를 수정해주세요</small>
+            <small v-else class="text-muted float-end">회원정보를 입력해주세요</small>
           </div>
           <div class="card-body">
             <form>
               <div class="row mb-3">
                 <label class="col-sm-2 col-form-label" for="basic-default-name">Username</label>
                 <div class="col-sm-4">
-                  <input type="text" class="form-control" id="basic-default-name" placeholder="penekhun">
+                  <input v-if="isEditMode" v-model.trim="account.username" disabled="true" type="text" class="form-control" id="basic-default-name" placeholder="penekhun">
+                  <input v-else type="text" class="form-control" id="basic-default-name" placeholder="penekhun">
                   <div class="form-text">also known as 'ID'</div>
                 </div>
 
                 <label class="col-sm-2 col-form-label" for="basic-default-name">Password</label>
                 <div class="col-sm-4">
-                  <input type="text" class="form-control" id="basic-default-name" placeholder="********">
-                  <div class="form-text">password 규칙</div>
+                  <input v-if="isEditMode" v-model.trim="account.password" type="text" class="form-control" id="basic-default-name" placeholder="********">
+                  <input v-else type="text" class="form-control" id="basic-default-name" placeholder="********">
+                  <div v-if="isEditMode" class="form-text"><strong>empty means no change</strong></div>
+                  <div v-else class="form-text">password 규칙</div>
                 </div>
               </div>
 
               <div class="row mb-3">
                 <label class="col-sm-2 col-form-label" for="basic-default-name">Nickname</label>
                 <div class="col-sm-4">
-                  <input type="text" class="form-control" id="basic-default-name" placeholder="PeNeKKKkkkkk">
+                  <input v-if="isEditMode" v-model.trim="account.nickname" type="text" class="form-control" id="basic-default-name" placeholder="PeNeKKKkkkkk">
+                  <input v-else type="text" class="form-control" id="basic-default-name" placeholder="PeNeKKKkkkkk">
                 </div>
 
                 <label class="col-sm-2 col-form-label" for="basic-default-name">RealName</label>
                 <div class="col-sm-4">
-                  <input type="text" class="form-control" id="basic-default-name" placeholder="MoonSeonghun">
+                  <input v-if="isEditMode" v-model.trim="account.realName" type="text" class="form-control" id="basic-default-name" placeholder="MoonSeonghun">
+                  <input v-else type="text" class="form-control" id="basic-default-name" placeholder="MoonSeonghun">
                 </div>
               </div>
 
@@ -43,24 +51,30 @@
                 <label class="col-sm-2 col-form-label" for="basic-default-email">Email</label>
                 <div class="col-sm-4">
                   <div class="input-group input-group-merge">
-                    <input type="text" id="basic-default-email" class="form-control" placeholder="penekhun@gmail.com" aria-label="penekhun@gmail.com" aria-describedby="basic-default-email2">
-<!--                    <span class="input-group-text" id="basic-default-email2">@example.com</span>-->
+                    <input v-if="isEditMode" v-model.trim="account.email" type="text" id="basic-default-email" class="form-control" placeholder="penekhun@gmail.com" aria-label="penekhun@gmail.com" aria-describedby="basic-default-email2">
+                    <input v-else type="text" id="basic-default-email" class="form-control" placeholder="penekhun@gmail.com" aria-label="penekhun@gmail.com" aria-describedby="basic-default-email2">
                   </div>
-                  <div class="form-text">You can use letters, numbers &amp; periods</div>
+                  <div class="form-text">like user@domain.com</div>
                 </div>
 
                 <label for="exampleFormControlSelect1" class="col-sm-2 col-form-label">User Role</label>
                 <div class="col-sm-4">
-                <select class="form-select" id="exampleFormControlSelect1" aria-label="Default select example">
-                  <option value="1">User</option>
-                  <option value="2">Admin</option>
+                <select v-if="isEditMode" v-model.trim="account.role" class="form-select" id="exampleFormControlSelect1" aria-label="Default select example">
+                  <option value="USER">User</option>
+                  <option value="ADMIN">Admin</option>
                 </select>
+                <select v-else class="form-select" id="exampleFormControlSelect1" aria-label="Default select example">
+                  <option value="USER">User</option>
+                  <option value="ADMIN">Admin</option>
+                </select>
+
                 </div>
               </div>
 
               <div class="row justify-content-end">
                 <div class="col-sm-12">
-                  <button type="submit" class="btn btn-primary">submit</button>
+                  <button v-if="isEditMode" type="button" @click="editEvent" class="btn btn-primary">Edit</button>
+                  <button v-else type="button" @click="makeEvent" class="btn btn-primary">submit</button>
                 </div>
               </div>
             </form>
@@ -74,11 +88,48 @@
 <script>
 
 import Content from "@/components/common/CommonContent";
+import {Account} from "@/api/account";
 
 export default {
-  name: "DefaultHome",
+  name: "CreateAccount",
   components: [
     Content,
-  ]
+  ],
+  props: {
+    editMode : Boolean,
+    account : {
+
+    },
+  },
+  methods:{
+    isEditMode(){
+      return this.editMode === true;
+    },
+
+    editEvent(){
+      // 계정 수정 이벤트
+      Account.editPartly(this.account).then((res) => {
+        console.log("response " ,res);
+        if (res.status === 200){
+          // alert("성공");
+          this.$router.go();
+        } else {
+          alert(res.data['detail']);
+        }
+      }).catch(err => {
+        alert("Error - " + err.response.data['detail']);
+        console.error(err);
+      })
+    },
+
+    makeEvent(){
+      // 계정 생성 이벤트
+    }
+  },
+  mounted() {
+    // if (this.isEditMode()){
+    //
+    // }
+  }
 }
 </script>
