adaa61e38a48c680895c3aa273f9ca8c05c39b63
PENEKhun <penekhun@gmail.com>
2022-07-06 09:30:02 +0900

    fix : Create Account가 생성/수정 컴포넌트로써 사용 가능 하게끔 픽스

--- src/components/home/CreateAccount.vue
+++ src/components/home/CreateAccount.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="container-xxl flex-grow-1 container-p-y">
-    <h4 v-if="!isEditMode" class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Accounts /</span> Create Account</h4>
+    <h4 v-if="!editMode" class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Accounts /</span> Create Account</h4>
 
     <div class="row">
       <!-- Basic Layout -->
       <div class="col-xxl">
         <div class="card mb-4">
           <div class="card-header d-flex align-items-center justify-content-between">
-            <h5 v-if="isEditMode" class="mb-0">Change the information to edit account</h5>
+            <h5 v-if="editMode" class="mb-0">Change the information to edit account</h5>
             <h5 v-else class="mb-0">Fill the blank to create account</h5>
 
-            <small v-if="isEditMode" class="text-muted float-end">정보를 수정해주세요</small>
+            <small v-if="editMode" class="text-muted float-end">정보를 수정해주세요</small>
             <small v-else class="text-muted float-end">회원정보를 입력해주세요</small>
           </div>
           <div class="card-body">
@@ -18,16 +18,15 @@
               <div class="row mb-3">
                 <label class="col-sm-2 col-form-label" for="basic-default-name">Username</label>
                 <div class="col-sm-4">
-                  <input v-if="isEditMode" v-model.trim="account.username" disabled="true" type="text" class="form-control" id="basic-default-name" placeholder="penekhun">
-                  <input v-else type="text" class="form-control" id="basic-default-name" placeholder="penekhun">
+                  <input v-model.trim="account.username" :disabled="editMode" type="text" class="form-control" id="basic-default-name" placeholder="penekhun">
+<!--                  <input v-else type="text" class="form-control" id="basic-default-name" placeholder="penekhun">-->
                   <div class="form-text">also known as 'ID'</div>
                 </div>
 
                 <label class="col-sm-2 col-form-label" for="basic-default-name">Password</label>
                 <div class="col-sm-4">
-                  <input v-if="isEditMode" v-model.trim="account.password" type="text" class="form-control" id="basic-default-name" placeholder="********">
-                  <input v-else type="text" class="form-control" id="basic-default-name" placeholder="********">
-                  <div v-if="isEditMode" class="form-text"><strong>empty means no change</strong></div>
+                  <input v-model.trim="account.password" type="text" class="form-control" id="basic-default-name" placeholder="********">
+                  <div v-if="editMode" class="form-text"><strong>empty means no change</strong></div>
                   <div v-else class="form-text">password 규칙</div>
                 </div>
               </div>
@@ -35,14 +34,12 @@
               <div class="row mb-3">
                 <label class="col-sm-2 col-form-label" for="basic-default-name">Nickname</label>
                 <div class="col-sm-4">
-                  <input v-if="isEditMode" v-model.trim="account.nickname" type="text" class="form-control" id="basic-default-name" placeholder="PeNeKKKkkkkk">
-                  <input v-else type="text" class="form-control" id="basic-default-name" placeholder="PeNeKKKkkkkk">
+                  <input  v-model.trim="account.nickname" type="text" class="form-control" id="basic-default-name" placeholder="PeNeKKKkkkkk">
                 </div>
 
                 <label class="col-sm-2 col-form-label" for="basic-default-name">RealName</label>
                 <div class="col-sm-4">
-                  <input v-if="isEditMode" v-model.trim="account.realName" type="text" class="form-control" id="basic-default-name" placeholder="MoonSeonghun">
-                  <input v-else type="text" class="form-control" id="basic-default-name" placeholder="MoonSeonghun">
+                  <input v-model.trim="account.realName" type="text" class="form-control" id="basic-default-name" placeholder="MoonSeonghun">
                 </div>
               </div>
 
@@ -51,7 +48,7 @@
                 <label class="col-sm-2 col-form-label" for="basic-default-email">Email</label>
                 <div class="col-sm-4">
                   <div class="input-group input-group-merge">
-                    <input v-if="isEditMode" v-model.trim="account.email" type="text" id="basic-default-email" class="form-control" placeholder="penekhun@gmail.com" aria-label="penekhun@gmail.com" aria-describedby="basic-default-email2">
+                    <input v-if="editMode" v-model.trim="account.email" type="text" id="basic-default-email" class="form-control" placeholder="penekhun@gmail.com" aria-label="penekhun@gmail.com" aria-describedby="basic-default-email2">
                     <input v-else type="text" id="basic-default-email" class="form-control" placeholder="penekhun@gmail.com" aria-label="penekhun@gmail.com" aria-describedby="basic-default-email2">
                   </div>
                   <div class="form-text">like user@domain.com</div>
@@ -59,11 +56,7 @@
 
                 <label for="exampleFormControlSelect1" class="col-sm-2 col-form-label">User Role</label>
                 <div class="col-sm-4">
-                <select v-if="isEditMode" v-model.trim="account.role" class="form-select" id="exampleFormControlSelect1" aria-label="Default select example">
-                  <option value="USER">User</option>
-                  <option value="ADMIN">Admin</option>
-                </select>
-                <select v-else class="form-select" id="exampleFormControlSelect1" aria-label="Default select example">
+                <select v-model.trim="account.role" class="form-select" id="exampleFormControlSelect1" aria-label="Default select example">
                   <option value="USER">User</option>
                   <option value="ADMIN">Admin</option>
                 </select>
@@ -73,7 +66,7 @@
 
               <div class="row justify-content-end">
                 <div class="col-sm-12">
-                  <button v-if="isEditMode" type="button" @click="editEvent" class="btn btn-primary">Edit</button>
+                  <button v-if="editMode" type="button" @click="editEvent" class="btn btn-primary">Edit</button>
                   <button v-else type="button" @click="makeEvent" class="btn btn-primary">submit</button>
                 </div>
               </div>
@@ -96,16 +89,25 @@ export default {
     Content,
   ],
   props: {
-    editMode : Boolean,
+    editMode : {
+      type : Boolean,
+      default : false
+    },
     account : {
-
+      type: Object,
+      default: function () {
+        return {
+          username : '',
+          password : '',
+          realName : '',
+          email : '',
+          role : 'USER',
+          nickname : '',
+        }
+      }
     },
   },
   methods:{
-    isEditMode(){
-      return this.editMode === true;
-    },
-
     editEvent(){
       // 계정 수정 이벤트
       Account.editPartly(this.account).then((res) => {
@@ -127,9 +129,7 @@ export default {
     }
   },
   mounted() {
-    // if (this.isEditMode()){
-    //
-    // }
+
   }
 }
 </script>
